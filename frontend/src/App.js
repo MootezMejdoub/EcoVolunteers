b1c96836b5312b405e33c648b979b05004f07d67
mootezmejdoub <mootez.mejdoub@esprit.tn>
2023-04-17 08:09:01 +0200

    tva

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import NavBar from "./components/navbar/NavBar";
 // import Home from "./pages/home/Home";
 // import Home from "./pages/Home";
-
+import wordsToNumbers from "words-to-numbers";
 import Login from "./pages/login/Login";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "./context/AuthContext";
 import Admin from "./pages/admin/Admin";
 import axios from "axios";
@@ -31,16 +31,26 @@ import ChatLive from "./components/chat/LiveChat";
 import io from "socket.io-client";
 import Profile from "./pages/profile/Profile";
 import EmailVerify from "./pages/EmailVerify";
+import useStyles from "./styles";
 
 import Chat from "./pages/Chat/Chat";
 import VideoCall from "./components/testvideocall/testvideocall";
 import Homee from "./pages/home/Homee";
 import Donation from "./components/Donation/Donation";
-
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { NewsCards } from "./components/NewsCards/NewsCards";
 function App() {
   const { auth, setAuth, logged, setLogged, socket, setSocket } =
     useContext(AuthContext);
   //const socket = io("http://localhost:5000"); // connect to the server
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const classes = useStyles();
+
+  const alanKey =
+    "719a4db1bd22c5579a847170b8b532b62e956eca572e1d8b807a3e2338fdd0dc/stage";
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -56,6 +66,52 @@ function App() {
           console.log(res);
           setAuth(res.data.user);
           setLogged(true);
+
+          alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+              if (command === "newz") {
+                navigate("/news");
+              }
+              if (command === "home") {
+                navigate("/home");
+              }
+              if (command === "chat") {
+                console.log("chat");
+                navigate("/chat");
+              }
+              if (command === "donation") {
+                navigate("/donate");
+              }
+              if (command === "myprofile") {
+                navigate(`/profile/${auth._id}`);
+              }
+              if (command === "newHeadlines") {
+                setNewsArticles(articles);
+                setActiveArticle(-1);
+              } else if (command === "instructions") {
+                setIsOpen(true);
+              } else if (command === "highlight") {
+                setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+              } else if (command === "open") {
+                const parsedNumber =
+                  number.length > 2
+                    ? wordsToNumbers(number, { fuzzy: true })
+                    : number;
+                const article = articles[parsedNumber - 1];
+
+                if (parsedNumber > articles.length) {
+                  alanBtn().playText("Please try that again...");
+                } else if (article) {
+                  console.log(article);
+                  window.open(article.url, "_blank");
+                  alanBtn().playText("Opening...");
+                } else {
+                  alanBtn().playText("Please try that again...");
+                }
+              }
+            },
+          });
         })
         .catch((error) => {
           console.log(error);
@@ -89,6 +145,15 @@ function App() {
                 <Route path="/chat" element={<Chat />} />
                 <Route path="/videcall" element={<VideoCall />} />
                 <Route path="/donate" element={<Donation />} />
+                <Route
+                  path="/news"
+                  element={
+                    <NewsCards
+                      newsArticles={newsArticles}
+                      activeArticle={activeArticle}
+                    />
+                  }
+                />
 
                 {/* <Route
                   path="/message"
--- frontend/src/pages/home/Homee.js
+++ frontend/src/pages/home/Homee.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./home.scss";
 import Share from "../../components/share/Share";
 
@@ -10,8 +10,30 @@ import io from "socket.io-client";
 import { useContext, useEffect } from "react";
 import AuthContext from "../../context/AuthContext";
 import ChatLive from "./../../components/chat/LiveChat";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useNavigate } from "react-router-dom";
 
+const alanKey =
+  "719a4db1bd22c5579a847170b8b532b62e956eca572e1d8b807a3e2338fdd0dc/stage";
 const Homee = () => {
+  const { auth, setAuth } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const [newsArticles, setNewsArticles] = useState([]);
+  useEffect(() => {
+    console.log(auth);
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        // if (command === "newHeadlines") {
+        //   console.log(articles);
+        //   setNewsArticles(articles);
+        // }
+      },
+    });
+  });
+
   return (
     <div style={{ display: "flex" }}>
       <LeftBar />
